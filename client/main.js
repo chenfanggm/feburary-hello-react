c041eb16562466a1d35f1ac0067e43d1208552b2
Chen Fang <chenfanggm@hotmail.com>
2018-02-11 22:17:02 -0800

    [hello world]

--- client/main.js
+++ client/main.js
@@ -1,15 +1,109 @@
-import './normalize'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
+const MOUNT_NODE = document.getElementById('root')
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      age: 20
+    }
+  }
+  
+  renderButton() {
+    return [1, 2, 3]
+  }
+
+  render() {
+    const { name } = this.props
+    const { age } = this.state
+    return (
+      <div>
+        Hello {name || 'User'} at age: {age}
+
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Hello name={'CHen'}></Hello>,
+  MOUNT_NODE
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import './normalize'
 import routes from './routes'
 import AppContainer from './containers/AppContainer'
+
+
+
+
+const helloWorld = (props) => {
+  return (
+    <div>Hello {props.name || 'User'}</div>
+  )
+}
+
+const AgeButton = ({ name, age }) => (
+  <button onClick={() => { window.alert(`User ${name} is at age ${age}!`) }}>
+    Age: {age}
+  </button>
+)
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      age: 20
+    }
+  }
+
+  render() {
+    const buttons = [1, 2, 3].map((age, index) => {
+      return (
+        <AgeButton key={index}
+                   name={this.props.children}
+                   age={age} />
+      )
+    })
+    return (
+      <div>
+        <div>Hello {this.props.children || 'User'}.</div>
+        {buttons}
+      </div>
+    )
+  }
+}
+
+
+
+
 
 
 // --------------------------------------
 // Render Setup
 // --------------------------------------
-const MOUNT_NODE = document.getElementById('root')
-
 let render = () => {
   ReactDOM.render(
     <AppContainer routes={routes} />,
@@ -49,4 +143,4 @@ if (__DEV__) {
 // --------------------------------------
 // Go!
 // --------------------------------------
-render()
+//render()
